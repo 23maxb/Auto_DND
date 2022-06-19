42a2fc5c0101245d0e908c87d124447d2154e0a8
Max Blennemann <23maxb@students.harker.org>
2022-06-18 21:06:15 -0700

    added remove to Effect.java

--- src/main/java/Status/Effect.java
+++ src/main/java/Status/Effect.java
@@ -7,19 +7,22 @@ import java.util.function.Consumer;
 public class Effect
 {
     private final Consumer<Player> applyEffect;
+    private final Consumer<Player> removeEffect;
     public String name;
     public String description;
     public int duration; //in turns
     public int turnsLeft;
     public Object Source;
 
-    public Effect(String name, String description, int duration, Object Source, Consumer<Player> applyEffect)
+    public Effect(String name, String description, int duration, Object Source,
+                  Consumer<Player> applyEffect, Consumer<Player> removeEffect)
     {
         this.name = name;
         this.description = description;
         this.duration = duration;
         this.Source = Source;
         this.applyEffect = applyEffect;
+        this.removeEffect = removeEffect;
     }
 
     /**
@@ -30,5 +33,15 @@ public class Effect
     public void apply(Player player)
     {
         applyEffect.accept(player);
+    }
+
+    /**
+     * Removes an effect from a player.
+     *
+     * @param player The player to remove the effect from.
+     */
+    public void remove(Player player)
+    {
+        removeEffect.accept(player);
     }
 }
